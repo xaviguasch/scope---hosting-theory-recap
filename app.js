1baf47a0019b6e35b02ea8fbb477cc6b2b118cf6
xaviguasch <xaviguasch@gmail.com>
2020-04-17 22:52:31 +0200

    added hoisting in action

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log(greet)
+// greet();
+printInfo()
+// console.log(hobbies);
+
+console.log(myName)
+
+var myName = 'Max'
+
+// function greet() {
+//   console.log('Hi there!');
+// }
+
+var greet = function () {
+  console.log('Hi there!')
+}
+
+function printInfo() {
+  console.log(hobbies)
+
+  var hobbies = ['Sports', 'Cooking']
+  console.log(hobbies)
+}
